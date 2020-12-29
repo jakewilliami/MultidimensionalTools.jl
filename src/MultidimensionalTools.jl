7bba994b5e5418cf1827a53a8fc6a7b59fa8c6f4
Jake W. Ireland <jakewilliami@icloud.com>
2020-12-30 12:41:59 +1300

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.13

---
+++ src/MultidimensionalTools.jl
@@ -0,0 +1,5 @@
+module MultidimensionalTools
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using MultidimensionalTools
+using Test
+
+@testset "MultidimensionalTools.jl" begin
+    # Write your tests here.
+end
