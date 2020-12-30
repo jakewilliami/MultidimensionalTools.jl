2cb41b5d5cb7d88d2afa2eed894cc5447099f27d
Jake W. Ireland <jakewilliami@icloud.com>
2020-12-30 14:58:38 +1300

    Added unit tests

--- src/MultidimensionalTools.jl
+++ src/MultidimensionalTools.jl
@@ -1,7 +1,7 @@
 module MultidimensionalTools
 
 # helper functions
-export n_adjacencies, extrema_indices, tryindex
+export n_adjacencies, extrema_indices, tryindex, get_directions
 # main functions
 export append_n_times, append_n_times_backwards, promote_to_nD, promote_to_3D,
     reshape_as_required, adjacencies, expanded_adjacencies, n_adjacent_to,
@@ -15,14 +15,59 @@ export append_n_times, append_n_times_backwards, promote_to_nD, promote_to_3D,
 Given a matrix or dimension, returns the number of elements adjacent to any given element in an infinite lattice/matrix (i.e., not including edges).  For edges, see ``).
 """
 n_adjacencies(dim::Int) = 3^dim - 1
-n_adjacencies(M::AbstractArray) = 3^ndims(M) - 1
+n_adjacencies(M::AbstractArray) = n_adjacencies(ndims(M))
+
+"""
+Returns an array of tuples of directions.
+
+```julia
+    get_directions(dim::Int; include_zero::Bool = false)
+
+julia> get_directions(3)
+27-element Array{Tuple{Int64,Int64,Int64},1}:
+ (-1, -1, -1)
+ (0, -1, -1)
+ (1, -1, -1)
+ (-1, 0, -1)
+ (0, 0, -1)
+ (1, 0, -1)
+ (-1, 1, -1)
+ (0, 1, -1)
+ (1, 1, -1)
+ (-1, -1, 0)
+ (0, -1, 0)
+ (1, -1, 0)
+ (-1, 0, 0)
+ (0, 0, 0)
+ (1, 0, 0)
+ (-1, 1, 0)
+ (0, 1, 0)
+ (1, 1, 0)
+ (-1, -1, 1)
+ (0, -1, 1)
+ (1, -1, 1)
+ (-1, 0, 1)
+ (0, 0, 1)
+ (1, 0, 1)
+ (-1, 1, 1)
+ (0, 1, 1)
+ (1, 1, 1)
+```
+"""
+function get_directions(dim::Int; include_zero::Bool = false)
+    𝟎 = ntuple(_ -> zero(Int), dim)
+    D = reshape(collect(Iterators.product([-1:1 for _ in 1:dim]...)), :)
+    return include_zero ? D : filter(d -> d ≠ 𝟎, D)
+end
+
+get_directions(M::AbstractArray; include_zero::Bool = false) = get_directions(ndims(M), include_zero = include_zero)
 
 """
     tryindex(M::Matrix{T}, inds::NTuple{N, Int}...)
 
 For each index specified, gets the index or nothing if the index is unavailable.
 """
-function tryindex(M::Matrix{T}, inds::NTuple{N, Int}...) where {T, N}
+function tryindex(M::Array{T, N}, inds::NTuple{N, Int}...) where {T, N}
     indices = Vector{Union{T, Nothing}}()
     
     for idx in inds
@@ -47,13 +92,13 @@ julia> T = [(3, 1), (4, 5), (6, 2), (1, 1)]
  (6, 2)
  (1, 1)
  
-julia> findmax_indices(T)
+julia> extrema_indices(T)
 1×2 Array{Tuple{Int64,Int64},2}:
 (1, 6)  (1, 5)
 ```
 """
 function extrema_indices(I::Union{Vector{NTuple{N, Int}}, Vector{CartesianIndex{N}}}) where N
-    return CartesianIndex.(extrema(reduce(vcat, permutedims(collect(i)) for i in I), dims = 1))
+    return vec(CartesianIndex.(extrema(reduce(vcat, permutedims(collect(i)) for i in I), dims = 1)))
 end
 """
 Repeats a specified value n many times along the specified dimension.
@@ -73,7 +118,7 @@ julia> append_n_times(A, 2, Int8(3), dims = 1) # repeat the value 3 twice along 
 ```
 """
 function append_n_times(M::Array{T, N}, n::Int, fill_elem::T; dims::Int = 1) where {T, N}
-    sz = ntuple(d -> ifelse(d == dims, n, size(M, d)), max(N, dims))
+    sz = ntuple(d -> d == dims ? n : size(M, d), max(N, dims))
     return cat(M, fill(fill_elem, sz); dims = dims)
 end
 
@@ -93,7 +138,7 @@ julia> append_n_times_backwards(A, 2, Int8(3), dims = 1) # repeat the value 3 tw
 ```
 """
 function append_n_times_backwards(M::Array{T, N}, n::Int, fill_elem::T; dims::Int = 1) where {T, N}
-    sz = ntuple(d -> ifelse(d == dims, n, size(M, d)), max(N, dims))
+    sz = ntuple(d -> d == dims ? n : size(M, d), max(N, dims))
     return cat(fill(fill_elem, sz), M; dims = dims)
 end
 
@@ -149,7 +194,7 @@ function reshape_as_required(M::Array{T, N}, expand_by::T, inds::Union{Vector{NT
         if ! all(map(m -> m ≤ size(M, d), ith_extrema))
             for invalid_idx in filter(i -> i > size(M, i) || i < 1, ith_extrema)
                 difference = invalid_idx - size(M, d)
-                M = ifelse(sign(difference) == 1, append_n_times(M, abs(difference), expand_by, dims = d), append_n_times_backwards(M, abs(difference), expand_by, dims = d))
+                M = sign(difference) == 1 ? append_n_times(M, abs(difference), expand_by, dims = d) : append_n_times_backwards(M, abs(difference), expand_by, dims = d)
             end
         end
     end
@@ -161,7 +206,7 @@ function reshape_as_required(M::Array{T, N}, expand_by::T, inds::Union{NTuple{N,
     return reshape_as_required(M, expand_by, Int[inds...])
 end
 
-function adjacencies(M::Matrix{T}, idx::NTuple{N, Int}) where {T, N}
+function adjacencies(M::Array{T, N}, idx::NTuple{N, Int}) where {T, N}
     𝟎 = ntuple(_ -> zero(Int), N)
     return T[k for k in tryindex(M, NTuple{N, Int}[idx .+ j for j in NTuple{N, Int}[t for t in Base.Iterators.product([-one(Int):one(Int) for i in one(Int):N]...)] if j ≠ 𝟎]...) if ! isnothing(k)]
 end
@@ -181,10 +226,15 @@ function expanded_adjacencies(M::Array{T, N}, expand_by::T, idx::Union{NTuple{N,
     return T[M[(i .+ idx_shift .- 1)...] for i in adjacent_indices]
 end
 
-function __inner_n_adjacent_to(adj_func::Function, M::Array{T, N}, expand_by::T, idx::Union{NTuple{N, Int}, CartesianIndex{N}}, adj_elem::T) where {N, T}
+"""
+    n_adjacent_to(M::Array{T, N}, idx::Union{NTuple{N, Int}, CartesianIndex{N}}, adj_elem::T)
+
+Given a matrix M, counts the number of adjacent elements to index `idx` that are exactly the `adj_elem`.
+"""
+function n_adjacent_to(M::Array{T, N}, idx::Union{NTuple{N, Int}, CartesianIndex{N}}, adj_elem::T) where {N, T}
     n = 0
     
-    for i in adj_func(M, expand_by, idx)
+    for i in adjacencies(M, idx)
         if i == adj_elem
             n += 1
         end
@@ -194,21 +244,20 @@ function __inner_n_adjacent_to(adj_func::Function, M::Array{T, N}, expand_by::T,
 end
 
 """
-    n_adjacent_to(M::Array{T, N}, idx::Union{NTuple{N, Int}, CartesianIndex{N}}, adj_elem::T)
-
-Given a matrix M, counts the number of adjacent elements to index `idx` that are exactly the `adj_elem`.
-"""
-function n_adjacent_to(M::Array{T, N}, idx::Union{NTuple{N, Int}, CartesianIndex{N}}, adj_elem::T) where {N, T}
-    return __inner_n_adjacent_to(adjacencies, M, expanded_by, idx, adj_elem)
-end
-
-"""
     expanded_n_adjacent_to(M::Array{T, N}, expand_by::T, idx::Union{NTuple{N, Int}, CartesianIndex{N}}, adj_elem::T)
 
 Given a matrix M, counts the number of adjacent elements to index `idx` that are exactly the `adj_elem`, expanding the matrix if needed (see `expanded_adjacencies`).
 """
 function expanded_n_adjacent_to(M::Array{T, N}, expand_by::T, idx::Union{NTuple{N, Int}, CartesianIndex{N}}, adj_elem::T) where {N, T}
-    return __inner_n_adjacent_to(expanded_adjacencies, M, expanded_by, idx, adj_elem)
+    n = 0
+    
+    for i in expanded_adjacencies(M, expand_by, idx)
+        if i == adj_elem
+            n += 1
+        end
+    end
+    
+    return n
 end
 
 """
@@ -216,8 +265,8 @@ end
 
 Returns the "globally" adjacent indices in arbitrary positions in the cardinal directions from a specified index in matrix `M`, ignoring certain adjacent elements (i.e., skipping over them).
 """
-function global_adjacencies_indices(M::Matrix{T}, idx::Union{NTuple{N, Int}, CartesianIndex{N}}, ignored_elem::T) where {T, N}
-    adjacent_indices, 𝟎 = Union{Vector{NTuple{N, Int}}, Vector{CartesianIndex{N}}}(), ntuple(_ -> zero(Int), N)
+function global_adjacencies_indices(M::Array{T, N}, idx::Union{NTuple{N, Int}, CartesianIndex{N}}, ignored_elem::T) where {T, N}
+    adjacent_indices, 𝟎 = Union{NTuple{N, Int}, CartesianIndex{N}}[], ntuple(_ -> zero(Int), N)
     directional_shifts = NTuple{N, Int}[i for i in NTuple{N, Int}[t for t in Base.Iterators.product([-one(Int):one(Int) for i in one(Int):N]...)] if i ≠ 𝟎]
     n_adjacent, adjacent_count = n_adjacencies(ndims(M)), 0
 
@@ -250,7 +299,7 @@ end
 
 Using `global_adjacencies_indices`, returns the elements of each globally adjacent index.  This function is mainly used for `global_n_adjacent_to`.
 """
-function global_adjacencies(M::Matrix{T}, idx::Union{NTuple{N, Int}, CartesianIndex{N}}, ignored_elem::T) where {N, T}
+function global_adjacencies(M::Array{T, N}, idx::Union{NTuple{N, Int}, CartesianIndex{N}}, ignored_elem::T) where {N, T}
     return T[M[i...] for i in global_adjacencies_indices(M, idx, ignored_elem)]
 end
 
@@ -260,7 +309,15 @@ end
 Given a matrix M, counts the number of adjacent elements to index `idx` that are exactly the `adj_elem`, skipping over certain elements if needed (see `expanded_adjacencies`).
 """
 function global_n_adjacent_to(M::Array{T, N},idx::Union{NTuple{N, Int}, CartesianIndex{N}}, ignored_elem::T) where {N, T}
-    return __inner_n_adjacent_to(global_adjacencies, M, expanded_by, idx, ignored_elem)
+    n = 0
+    
+    for i in global_adjacencies(M, idx, ignored_elem)
+        if i == adj_elem
+            n += 1
+        end
+    end
+    
+    return n
 end
 
 end # end module
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,9 +6,66 @@
 
 include(joinpath(dirname(dirname(@__FILE__)), "src", "MultidimensionalTools.jl"))
 
-using .ComputabilityTheory
+using .MultidimensionalTools
 using Test
 
+function aresamearrays(A::AbstractVector{T}, B::AbstractVector{R}) where {T, R}
+	A === B && return true
+	length(A) ≠ length(B) && return false
+	
+	for (a, b) in zip(sort(A), sort(B))
+		if a ≠ b
+			return false
+		end
+	end
+	
+	return true
+end
+
+"""
+Given two arrays A and B, ensures they have the same elements
+"""
+function aresamearrays(A::AbstractArray{T, N}, B::AbstractArray{R, M}) where {T, R, N, M}
+	A === B && return true
+	length(A) ≠ length(B) && return false
+	
+	for (a, b) in zip(sort(reshape(A, :)), sort(reshape(B, :)))
+	# for (a, b) in zip(sort(A, dims = 1), sort(B, dims = 1))
+		if a ≠ b
+			return false
+		end
+	end
+	
+	return true
+end
+
 @time @testset "MultidimensionalTools.jl" begin
-    
+    @test n_adjacencies(2) == 8
+    @test n_adjacencies(3) == 26
+    @test n_adjacencies(17) == 129140162
+    @test aresamearrays(get_directions(3), [(-1, -1, -1), (0, -1, -1), (1, -1, -1), (-1, 0, -1), (0, 0, -1), (1, 0, -1), (-1, 1, -1), (0, 1, -1), (1, 1, -1), (-1, -1, 0), (0, -1, 0), (1, -1, 0), (-1, 0, 0), (1, 0, 0), (-1, 1, 0), (0, 1, 0), (1, 1, 0), (-1, -1, 1), (0, -1, 1), (1, -1, 1), (-1, 0, 1), (0, 0, 1), (1, 0, 1), (-1, 1, 1), (0, 1, 1), (1, 1, 1)])
+	A = Int[-69 -63 -5; 119 67 1; -101 7 -88]
+	@test tryindex(A, (1, 1), (2, 2), (3, 3), (4, 4), (5, 5)) == [-69, 67, -88, nothing, nothing]
+	T = [(3, 1), (4, 5), (6, 2), (1, 1)]
+	@test extrema_indices(T) == CartesianIndex.([(1, 6), (1, 5)])
+	@test append_n_times(A, 5, 1, dims = 1) == [-69 -63 -5; 119 67 1; -101 7 -88; 1 1 1; 1 1 1; 1 1 1; 1 1 1; 1 1 1]
+	@test append_n_times(A, 5, 1, dims = 2) == [-69 -63 -5 1 1 1 1 1; 119 67 1 1 1 1 1 1; -101 7 -88 1 1 1 1 1]
+	@test append_n_times_backwards(A, 5, 1, dims = 1) == [1 1 1; 1 1 1; 1 1 1; 1 1 1; 1 1 1; -69 -63 -5; 119 67 1; -101 7 -88]
+	@test append_n_times_backwards(A, 5, 1, dims = 2) == [1 1 1 1 1 -69 -63 -5; 1 1 1 1 1 119 67 1; 1 1 1 1 1 -101 7 -88]
+	@test promote_to_nD(A, 3, 0) == cat([0 0 0; 0 0 0; 0 0 0], [-69 -63 -5; 119 67 1; -101 7 -88], [0 0 0; 0 0 0; 0 0 0], dims = 3)
+	@test promote_to_3D(A, 0) == cat([0 0 0; 0 0 0; 0 0 0], [-69 -63 -5; 119 67 1; -101 7 -88], [0 0 0; 0 0 0; 0 0 0], dims = 3)
+	@test reshape_as_required(A, 0, [(1, 1), (2, 2), (3, 3), (4, 4), (5, 5)]) == [-69 -63 -5 0 0; 119 67 1 0 0; -101 7 -88 0 0; 0 0 0 0 0; 0 0 0 0 0]
+	# @test reshape_as_required(A, 0, [(1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (-2, -2)]) == [-69 -63 -5 0 0; 119 67 1 0 0; -101 7 -88 0 0; 0 0 0 0 0; 0 0 0 0 0]
+	@test aresamearrays(adjacencies(A, (2, 2)), [-69, 119, -101, -63, 7, -5, 1, -88])
+	@test aresamearrays(adjacencies(A, (3, 3)), [67, 7, 1])
+	@test aresamearrays(expanded_adjacencies(A, 0, (2, 2)), [-69, 119, -101, -63, 7, -5, 1, -88])
+	@test aresamearrays(expanded_adjacencies(A, 0, (3, 3)), [67, 7, 0, 1, 0, 0, 0, 0])
+	@test n_adjacent_to(A, (2, 2), 7) == 1
+	@test n_adjacent_to(A, (3, 3), 7) == 1
+	@test n_adjacent_to(promote_to_3D(A, 7), (3, 3, 1), 7) == 4
+	@test expanded_n_adjacent_to(A, 7, (3, 3), 7) == 6
+	@test expanded_n_adjacent_to(A, 7, (6, 6), 7) == 8 # (the filled element is surrounding the given index)
+	@test expanded_n_adjacent_to(promote_to_3D(A, 7), 7, (3, 3, 1), 7) == 24
+	@test aresamearrays(global_adjacencies_indices(A, (2, 2), 7), [(1, 1), (2, 1), (3, 1), (1, 2), (1, 3), (2, 3), (3, 3)])
+	@test aresamearrays(global_adjacencies_indices(promote_to_3D(A, 7), (3, 3, 1), 7), [(2, 2, 2), (2, 3, 2), (3, 3, 2)])
 end # end testset
